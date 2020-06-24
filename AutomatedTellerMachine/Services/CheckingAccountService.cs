4d80a3f33bb2225cac32f88b44348c2959668d09
Farook M Fazrin <nirzaf1@hotmail.com>
2020-06-24 07:14:35 +0530

    Add project files.

---
+++ AutomatedTellerMachine/Services/CheckingAccountService.cs
@@ -0,0 +1,26 @@
+﻿using AutomatedTellerMachine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Services
+{
+    public class CheckingAccountService
+    {
+        private ApplicationDbContext db;
+
+        public CheckingAccountService(ApplicationDbContext dbContext)
+        {
+            db = dbContext;
+        }
+
+        public void CreateCheckingAccount(string firstName, string lastName, string userId, decimal initialBalance)
+        {           
+            var accountNumber = (123456 + db.CheckingAccounts.Count()).ToString().PadLeft(10, '0');
+            var checkingAccount = new CheckingAccount { FirstName = firstName, LastName = lastName, AccountNumber = accountNumber, Balance = initialBalance, ApplicationUserId = userId };
+            db.CheckingAccounts.Add(checkingAccount);
+            db.SaveChanges();
+        }
+    }
+}
